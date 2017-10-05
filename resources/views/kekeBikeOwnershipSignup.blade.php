d73b40f2094f9843074e8aaa0aec417c25de6fb0
Covenant <CovenantChukwudi@gmail.com>
2017-10-05 18:18:23 +0100

    modified select bank

--- resources/views/kekeBikeOwnershipSignup.blade.php
+++ resources/views/kekeBikeOwnershipSignup.blade.php
@@ -323,7 +323,7 @@
                                             <div class="col-md-12">
                                                 <label><strong>Bank Paid Into</strong></label>
                                             </div>
-                                            <div class="col-md-9 col-sm-9 col-xs-9">
+                                            <div class="col-md-12 col-sm-12 col-xs-12">
                                                 <select name="bankPaidInto" class="form-control input-lg">
                                                     <option value="0" selected>Select Bank</option>
                                                     <option value="GT Bank PLC">GT Bank PLC</option>
