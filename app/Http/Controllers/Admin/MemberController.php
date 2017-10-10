d3065cb50889bb8652d41847b20c572a2dbb9a0f
makinde teniola <brainiacten@gmail.com>
2017-10-10 17:41:23 +0100

    bolo

--- app/Http/Controllers/Admin/MemberController.php
+++ app/Http/Controllers/Admin/MemberController.php
@@ -24,7 +24,7 @@ class MemberController extends Controller
     public function show($id){
         $member = $this->memberService->getMemberById($id);
         $portfolio = $this->portfolioService->getPortfolioById($id);
-        return view('admin.singlemember')->with('member', $member)->('portfolio', $portfolio);
+        return view('admin.singlemember')->with(['member', $member, 'portfolio', $portfolio]);
     }
     
     /**
