cdade02187a76adfa529ee27c3a24b69ff16d95b
makinde teniola <brainiacten@gmail.com>
2017-10-10 17:58:43 +0100

    bolo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -160,7 +160,7 @@
                     </div>
                 </div>
 
-                
+
                     <div class="pages" id="features">
 
                         <div class="">
@@ -176,22 +176,7 @@
                         </div>
                     </div>
                     
-                    <div class="pages visible-xs" id="features">
-
-                        <div class="padding-tb75">
-                            <div class="container">
-                                <div class="row">
-                                     <div class="col-md-6 text-align-center" data-appear-animation="fadeInLeftBig" data-appear-animation-delay=".2"> <img src="images/scheme-02.png" alt=""> </div>
-
-                                    <div class="col-md-4 col-md-offset-1 inverse-margin" data-appear-animation="fadeInRightBig" data-appear-animation-delay="200">
-                                        <h2>Financial Empowerment Scheme.</h2>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Suspendisse consectetur fringilla suctus. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-                                        <a href="{{ url('/financialempowermentsignup') }}" class="btn btn-primary btn-lg">Register For This Scheme</a> </div>
-                                   
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    
 
                     <div class="pages hidden-xs" id="features">
 
@@ -204,7 +189,7 @@
                                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Suspendisse consectetur fringilla suctus. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
                                         <a href="{{ url('/financialempowermentsignup') }}" class="btn btn-primary btn-lg">Register For This Scheme</a> </div>
                          
-                                     <div class="col-md-6 text-align-center" data-appear-animation="fadeInLeftBig" data-appear-animation-delay=".2"> <img src="images/bike.jpg" alt=""> </div>
+                                     <div class="col-md-6 text-align-center" data-appear-animation="fadeInLeftBig" data-appear-animation-delay=".2"> <img src="images/scheme-02.png" alt=""> </div>
           
                                 </div>
                             </div>
