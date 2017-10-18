407394a92bad9bf74a3594d1b03b10def5dabd5a
Chinelo <nelokwaz@gmail.com>
2017-10-18 15:48:58 +0100

    Pushed new changes again

--- database/migrations/2017_10_17_184547_add_account_to_members_table.php
+++ database/migrations/2017_10_17_184547_add_account_to_members_table.php
@@ -13,7 +13,7 @@ class AddAccountToMembersTable extends Migration
      */
     public function up()
     {
-        Schema::table('members', function (Blueprint $table) {
+        Schema::table('Members', function (Blueprint $table) {
             $table->string('account_name')->nullable();
             $table->string('account_no')->nullable();
         });
