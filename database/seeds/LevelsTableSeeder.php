c9c666df2c30b4fbf2559bf0dadb65687c81a686
Chinelo <nelokwaz@gmail.com>
2017-11-03 10:19:55 +0100

    Pushed changes made

--- database/seeds/LevelsTableSeeder.php
+++ database/seeds/LevelsTableSeeder.php
@@ -11,7 +11,7 @@ class LevelsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('Levels')->insert(array(
+        DB::table('levels')->insert(array(
             array('description' => 'Classic','amount' => 3000),
             array('description' => 'Premium','amount' => 5000),
             array('description' => 'Platinum', 'amount' => 10000)
