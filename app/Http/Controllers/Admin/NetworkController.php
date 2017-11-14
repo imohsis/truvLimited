3d7d4d535682c9bcd37d187a5f62ee5c9fbc87bc
chinelo <nelokwaz@gmail.com>
2017-11-14 10:20:53 +0100

    Edited pagination

--- app/Http/Controllers/Admin/NetworkController.php
+++ app/Http/Controllers/Admin/NetworkController.php
@@ -36,7 +36,7 @@ class NetworkController extends Controller
             ->where('refered_by', '=', $id)
             ->where('approved_status', '=', 1)
             ->orderBy('Portfolios.created_at','desc')
-            ->paginate('15');
+            ->get();
 
         $name = Member::select('full_name')
                 ->where('id', '=', $id)
