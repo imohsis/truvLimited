88f8a3015f74b22e3bb2d09f41aaa4c4e1896edb
chinelo <nelokwaz@gmail.com>
2017-11-17 13:45:22 +0100

    Pushed changes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -40,7 +40,7 @@
                             <?php $backgroundColor = "#EDF1F5"; ?>
                             @endif
                         <tr style="background-color: {{ $backgroundColor}}};">
-                            <td>{{$portfolio->count()}}</td>
+                            <td>{{count($awaitingPortfolios)}}</td>
                             <td class="txt-oflo"> &nbsp;{{ $member->full_name }}</td>
                             <td class="">{{ $member->user()->email }}</td>
                             <td class="">{{ $member->phone }}</td>
