ed329b22c1fcf32d2e3cd98b17136cb4b55ffd62
chinelo <nelokwaz@gmail.com>
2017-11-17 12:57:34 +0100

    Pushed changes

--- resources/views/emails/deactivate.blade.php
+++ resources/views/emails/deactivate.blade.php
@@ -5,6 +5,7 @@
 <h4>We are really sorry to say that your account has been deactivated for the following reason(s):</h4>
 <h3>{{$reason}}</h3>
 <h4>To resolve this, you can reach us via 08149653352 or 09082816181 or via email on truvlimited@gmail.com</h4>
+<h4>Or visit our website <a href="{{getenv('APP_URL')}}">www.truvlimited.com</a></h4>
 <h4>Many thanks,</h4>
 <h4>Tru V Support Team</h4>
 </body>
