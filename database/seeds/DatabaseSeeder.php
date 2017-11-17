ede44c3efe625319e8473aa08d83deac71837f2d
chinelo <nelokwaz@gmail.com>
2017-11-17 15:33:14 +0100

    Pushed changes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,9 +14,16 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         
         DB::table('users')->insert([
-            'name' => 'Thinksihfts',
-            'email' => 'thinkshifts@gmail.com',
+            'name' => 'Tru V',
+            'email' => 'truvthinkshifts@gmail.com',
             'password' => bcrypt('Thinkshifts2020'),
+            'role_id' => App\UserRole::$ADMINISTRATOR
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Truv Limited',
+            'email' => 'truvlimited@gmail.com',
+            'password' => bcrypt('Truvlimited2020'),
             'role_id' => App\UserRole::$ADMINISTRATOR
         ]);
 
